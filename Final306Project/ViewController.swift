9fde4ba229cd52aa3661c07d7e48d4e42f926c0a
hansen12345 <Alex@Alexs-MacBook-Pro.local>
2017-02-26 18:25:03 -0500

    login
    login

--- Final306Project/ViewController.swift
+++ Final306Project/ViewController.swift
@@ -8,98 +8,52 @@
 
 import UIKit
 import SwiftyJSON
+import Foundation
+
 
 class ViewController: UIViewController {
-
-    @IBOutlet var employeeID: UITextField!
-    @IBOutlet var employeePassword: UITextField!
     
-    
-    var numberOfRows = 0
-    var employeeArray = [String]()
-    var passwordArray = [String]()
-    
-    override func viewDidLoad() {
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //parseJSON()
-
-    }
-    
-    @IBAction func loginButton(_ sender: Any) {
-        let username = self.employeeID.text
-        let password = self.employeePassword.text
-        
-        let path : String = Bundle.main.path(forResource: "loginUser", ofType: "json") as String!
-        let loginData = NSData(contentsOfFile: path) as NSData!
-        let readableJSON = JSON(data: loginData as! Data, options: JSONSerialization.ReadingOptions.mutableContainers, error: nil)
-        
-        numberOfRows = readableJSON["users"].count
-        
-        for i in 1...numberOfRows {
-            var user = "user"
-            user += "\(i)"
-            
-            let loginID = readableJSON["users"][user]["employeeID"].string as String!
-            let passwordID = readableJSON["users"][user]["password"].string as String!
-            
-            employeeArray.append(loginID!)
-            passwordArray.append(passwordID!)
-            
-            
-        }
-        NSLog("\(employeeArray)")
-        NSLog("\(passwordArray)")
-        NSLog("\(numberOfRows)")
-        NSLog("\(username)")
-        NSLog("\(password)")
-        if ((username?.isEmpty)! || (password?.isEmpty)!) {
-            let alertController = UIAlertController(title: "INVALID", message: "All fields are required", preferredStyle: .alert)
-            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) {
-                action in
-            }
-            alertController.addAction(cancelAction)
-            let okAction = UIAlertAction(title: "OK", style: .default) {
-                action in
-            }
-            alertController.addAction(okAction)
-            self.present(alertController, animated: true) {
-            }
-            return
-        }
-        
-        var b = 0
-        while (b < numberOfRows) {
-           
-            if (username == employeeArray[b] && password == passwordArray[b]) {
-                NSLog("LOGGED IN")
-                return
-            } else {
-                 b += 1
-            }
-        }
-        let alertController = UIAlertController(title: "INVALID", message: "Username and password do not match", preferredStyle: .alert)
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) {
-            action in
-        }
-        alertController.addAction(cancelAction)
-        let okAction = UIAlertAction(title: "OK", style: .default) {
-            action in
-        }
-        alertController.addAction(okAction)
-        self.present(alertController, animated: true) {
-        }
-
-        
-        
-        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    @IBOutlet var UN: UITextField!
+    @IBOutlet var PW: UITextField!
+    @IBAction func loginButton(_ sender: Any) {
+        let URL_login = "Http://192.168.99.163/SMARTRoom.php"
+        let requestURL = NSURL(string: URL_login)
+        let request = NSMutableURLRequest(url: requestURL as! URL)
+        request.httpMethod = "POST"
+        let userNAME = UN.text
+        let passWORD = PW.text
+        let postParameters = "Username=" + userNAME! + "& Password=" + passWORD!;
+        request.httpBody = postParameters.data(using: String.Encoding.utf8)
+        let task = URLSession.shared.dataTask(with: request as URLRequest){
+            data, response, error in
+            
+            if error != nil{
+                print("error is \(error)")
+                return;
+            }
+            do{
+                let myJSON = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? [String: Any]
+                if let parseJSON = myJSON {
+                    var msg : String!
+                    msg = parseJSON["message"] as! String?
+                    print(msg)
+                }
+            } catch{
+                print(error)
+            }
+        }
+        
+        task.resume()
+    }
+    
 }
+
+
 
 
